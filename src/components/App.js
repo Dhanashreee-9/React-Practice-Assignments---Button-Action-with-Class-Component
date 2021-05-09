0330b449d714b74eafb3c5e98961e73a1e042733
Dhanashreee-9 <68267346+Dhanashreee-9@users.noreply.github.com>
2021-05-09 17:42:07 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,24 @@
 import React, {Component, useState} from "react";
-import '../styles/App.css';
+import "./../styles/App.css";
 
 class App extends Component {
-    constructor(props) {
-		super(props);
-	};
+	render(){
+	 const [showpara, showParagraph] = useState(false);
 
-    render() {
-    	return(
-    		<div id="main">
-				{ /* Do not remove this main div!! */ }
-    		</div>
-    	);
-    }
-}
+    const onClicking = () => {
+        showParagraph(true);
+    };
+    return (
+        <div id="main">
+            {/* Do not remove this main div!! */}
+            <button id="click" onClick={() => onClicking()}>
+                {"Click me"}
+            </button>
+            {showpara ? <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p> : ""}
+        </div>
+    );}
+   
+};
 
 
 export default App;
