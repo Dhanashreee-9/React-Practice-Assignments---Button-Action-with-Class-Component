5c4b544d8ff9d6b40d5083b05775a21b11cfce80
Dhanashreee-9 <68267346+Dhanashreee-9@users.noreply.github.com>
2021-05-10 20:29:51 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -6,17 +6,17 @@ class App extends Component {
 	constructor(){
 	super();
 		this.state={
-	        para: true,
+	        para: false, // initially ye false rahega to p tag show nhi hoga
 		}
 	}
 	 changePara=()=>{
-		this.setState({ para :"Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"})
+		this.setState({ para :true}); // click krne ke baad para true set ho gaya so p this.state.para true hoga and p tag show krega
 	}		
 	render(){
         return(
         <div>
     	   <button id ="click" onClick={this.changePara}>Click Me!!</button>
-           { this.changePara ? <p id="para">{this.state.para}</p> : ""}
+           { this.state.para ? <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p> : ""}
         </div>
 	)
 };
